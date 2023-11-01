cce8a0f3b6647716a516eacfdfbdeb655f5ce55e
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-01 00:19:56 +0000

    Add task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    print("{:02}".format(i), end=" ")
-
-print()
+    if i < 99:
+        print("{:02},".format(i), end=" ")
+    else:
+        print("{}".format(i))
