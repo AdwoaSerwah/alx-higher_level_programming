822a19d1d9908873a2ecea6fb6364c958c9f7365
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-01 00:58:09 +0000

    Add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = ord(i) - 32
+            print("{:c}".format(i), end="")
+        else:
+            print("{}".format(i), end="")
+    print()
