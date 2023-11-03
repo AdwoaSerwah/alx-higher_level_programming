b3db0a6807d8863a3bbb47ee0413a9c857c7affd
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-03 01:49:23 +0000

    Add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    argv_len = len(argv) - 1
+    if argv_len > 1:
+        arg = "arguments:"
+    elif argv_len == 1:
+        arg = "argument:"
+    else:
+        arg = "argument."
+    print("{} {}".format(argv_len, arg))
+    for i in range(1, argv_len + 1):
+        print("{}: {}".format(i, argv[i]))
