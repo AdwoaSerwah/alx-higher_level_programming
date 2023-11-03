9f9efbbf3055b6bec00a040fb672ea6f2297c1e8
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-03 02:45:28 +0000

    Add task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for i in range(len(names)):
-        print(names[i])
+    for i in names:
+        if not name.startswith("__"):
+            print(i)
