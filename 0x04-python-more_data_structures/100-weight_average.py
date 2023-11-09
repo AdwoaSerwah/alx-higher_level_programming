668eececdb4f6c38765ff518e93f78d5de2a85cf
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-09 05:29:19 +0000

    Add task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    t_score = 0
+    t_wgt = 0
+
+    for score, weight in my_list:
+        t_score += score * weight
+        t_wgt += weight
+
+    if t_wgt == 0:
+        return 0
+
+    weighted_avg = t_score / t_wgt
+    return weighted_avg
