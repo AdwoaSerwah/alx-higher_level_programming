ff93f3e5668c2f80be2dc58b2f64a898ead6931f
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-28 06:32:23 +0000

    Add task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -30,7 +30,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
         """
         Computes and returns the area of a square
