5fafc76a91c91f882c052a8cc27cc6065fd1bd5e
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-28 18:18:00 +0000

    Add task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""This module represents class LockedClass"""
+
+
 class LockedClass:
+    """This defines a LockedClass"""
     def __setattr__(self, name, value):
+        """
+        Sets first name attribute
+
+        Args:
+            name (str): The name
+            value: The value
+
+        Raises:
+            AttributeError: If attribute is not first_name
+        """
         if name != 'first_name':
             raise AttributeError(
                     "'LockedClass' object has no attribute '{}'".format(name))
