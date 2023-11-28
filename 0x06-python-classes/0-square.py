a92d137f8411f5a001e87815b89cf8805e54284c
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-11-28 05:21:48 +0000

    Add task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class represents a square and is used as a base class for
+more specific types of squares. It has no attributes or methods defined.
+"""
+class Square:
+    """
+    This class represents a square.
+
+    It can be used as a base class for more specific types of squares.
+    """
+    pass
