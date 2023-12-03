4a294994932b730cb1dcea077db8abe88eec1130
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-03 05:11:10 +0000

    Add task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Tis module defines the function print_square"""
+
+
+def print_square(size=0):
+    """
+    Prints a square with the character #
+
+    Args:
+        size (int): Size length of the square
+
+    Raises:
+        TypeError: if size is not an integer or size is float and < 0
+        ValueError: if size < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print("")
+    else:
+        [print("#" * size) for i in range(size)]
