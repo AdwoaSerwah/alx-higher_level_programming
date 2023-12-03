a2ce996d81c1c2cf7d074b44e56be4af03cd3ef3
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-03 04:20:37 +0000

    Add task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,20 @@
 
 
 def say_my_name(first_name="default", last_name=""):
+    """
+    Says your name
+
+    Args:
+        first_name (str): First Name
+        last_name (str): Last Name
+
+    Raises:
+        TypeError: If first or last name is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     first_name = first_name.strip()
     last_name = last_name.strip()
-    print("My name is {} {}".format(first_name, last_name).strip())
+    print("My name is {} {}".format(first_name, last_name))
