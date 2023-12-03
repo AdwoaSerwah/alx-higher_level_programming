d6a1e2f9bc6fcd2cb7789c4886bdd330e48e2991
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-03 16:53:24 +0000

    Add task 5

---
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to find the max integer in a list
+"""
+
+
+def max_integer(lst=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if not isinstance(lst, list):
+        raise TypeError("lst must be a list of integers")
+    if not all(isinstance(m, int) for m in lst):
+        raise TypeError("lst must be a list of integers")
+    if len(lst) == 0:
+        return None
+    result = lst[0]
+    i = 1
+    while i < len(lst):
+        if lst[i] > result:
+            result = lst[i]
+        i += 1
+    return result
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+
+    """Tests the function max_integer"""
+    def test_type(self):
+        """Check if data type is list"""
+        self.assertRaises(TypeError, max_integer, 1)
+        self.assertRaises(TypeError, max_integer, ["w"])
+        self.assertRaises(TypeError, max_integer, [1, "w"])
+        self.assertRaises(TypeError, max_integer, [1, []])
+        self.assertRaises(TypeError, max_integer, [[]])
+        self.assertRaises(TypeError, max_integer, "w")
+
+    def test_empty_list(self):
+        """Test if list is empty"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_no_args(self):
+        """Test if no argument"""
+        self.assertEqual(max_integer(), None)
+
+    def test_integers(self):
+        """Test integers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([1, 1]), 1)
+        self.assertEqual(max_integer([-1, - 2, -3, -4]), -1)
+        self.assertEqual(max_integer([2]), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
