012c73e12d526a9f031c9d8fe781033fdda04230
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-04 06:15:40 +0000

    m

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -15,12 +15,17 @@ def lazy_matrix_mul(m_a=[[1]], m_b=[[1]]):
     Returns:
         list of lists: New matrix
     """
-    np_a = np.array(m_a)
-    np_b = np.array(m_b)
-
-    new = np_a @ np_b
-
-    new = new.tolist()
-
-    result_str = np.array(new)
-    return result_str
+    try:
+        np_a = np.array(m_a)
+        np_b = np.array(m_b)
+        new = np_a @ np_b
+        new = new.tolist()
+        result_str = np.array(new)
+        return result_str
+    except ValueError as ve:
+        raise
+    except TypeError as te:
+        if "Object arrays are not currently supported" in str(te):
+            raise ValueError("setting an array element with a sequence")
+        else:
+            raise
