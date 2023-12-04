e8bde0a8b924726006d7fea68bfe9c15d021e068
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-04 07:17:36 +0000

    102

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,41 +1,34 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 /**
  * print_python_string - Prints information about Python strings
- * @p: PyObject pointer to the Python string
+ * @p: PyObject string
  */
-void print_python_string(PyObject *p)
-{
-	printf("[.] string object info\n");
-
-	if (!PyUnicode_Check(p))
+void print_python_string(PyObject *p) {
+	if (PyUnicode_Check(p))
 	{
+		Py_ssize_t length;
+		wchar_t *wstr;
+
+		length = PyUnicode_GET_LENGTH(p);
+		wstr = PyUnicode_AsWideCharString(p, &length);
+
+		printf("[.] string object info\n");
+		printf("  type: ");
+		if (PyUnicode_IS_COMPACT_ASCII(p))
+			printf("compact ascii\n");
+		else
+			printf("compact unicode object\n");
+
+		printf("  length: %ld\n", length);
+		printf("  value: %s\n", PyUnicode_AsUTF8(p));
+
+		PyMem_Free(wstr);
+	}
+	else
+	{
+		printf("[.] string object info\n");
 		printf("  [ERROR] Invalid String Object\n");
-		return;
 	}
-
-	Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-
-	printf("  type: compact ");
-	if (PyUnicode_IS_ASCII(p))
-		printf("ascii\n");
-	else
-		printf("unicode object\n");
-
-	printf("  length: %ld\n", length);
-	printf("  value: ");
-
-	const char *utf8_str = PyUnicode_AsUTF8(p);
-
-	if (utf8_str != NULL)
-	{
-		for (Py_ssize_t i = 0; i < length; ++i)
-			printf("%c", utf8_str[i]);
-	}
-	else
-	{
-		printf("[ERROR] Failed to decode UTF-8\n");
-	}
-
-	printf("\n");
 }
