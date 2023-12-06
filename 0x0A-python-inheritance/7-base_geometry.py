c6a49943b35c3647ffdd741789ebcc5ef4dd9efb
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-06 18:05:40 +0000

    Revert "Add"
    This reverts commit e44f86e8a3b3e2ddf5e832651762b2ed65326594.

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         """Raises an exception"""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
+    def integer_validator(self, name, value=1):
         """
         Validates value
 
