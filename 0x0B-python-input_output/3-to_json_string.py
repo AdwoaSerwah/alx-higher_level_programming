9b5c0473fb86f59ad7ace8aa317704e8bcf4e2c0
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-06 23:25:39 +0000

    Add task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a function called to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string)
+
+    Args:
+        obj: Object
+
+    Returns:
+        str: json representation of an object
+    """
+    return json.dumps(my_obj)
