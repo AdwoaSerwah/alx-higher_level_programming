89c268758ba6fe5b5b9efd9d74852e351faaf169
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-09 15:55:29 +0000

    Add task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This modules defines a Student class"""
+
+
+class Student:
+    """This represents an instance of Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = {}
+        obj = self.__dict__
+        if type(attrs) is list and all((type(i) is str) for i in attrs):
+            for k, v in obj.items():
+                if k in attrs:
+                    new_dict[k] = v
+            return new_dict
+
+        for key, value in obj.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                new_dict[key] = value
+        return new_dict
