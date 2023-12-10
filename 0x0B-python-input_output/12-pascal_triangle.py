ab42c2b0427aeffb4b451cef2444fc56ca8f7b54
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2023-12-10 23:21:46 +0000

    Add task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a function called pascal_triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+
+    new_list = [[1], [1, 1]]
+    n = n - 2
+    for i in range(n):
+        tmp = [1]
+        for j in range(len(new_list[i + 1]) - 1):
+            sum = new_list[i + 1][j] + new_list[i + 1][j + 1]
+            tmp.append(sum)
+        tmp.append(1)
+        new_list.append(tmp)
+    return new_list
