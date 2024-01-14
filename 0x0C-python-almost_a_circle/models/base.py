fbb72356d94614175ef7638d167ecfd01d650226
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-01-14 23:38:54 +0000

    Add task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """This module represents Base class"""
+import json
 
 
 class Base:
     """This defines a Base instance"""
     __nb_objects = 0
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
     def __init__(self, id=None):
         if id is not None:
