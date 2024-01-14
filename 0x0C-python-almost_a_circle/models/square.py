d8cc6410545c069a382d02495a37d020a7454ee6
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-01-14 17:50:07 +0000

    Add task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This defines an instance of this class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
