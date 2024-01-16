7e131bb42d605b726fce06487e5ab8567dcfa7fb
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-01-16 22:21:51 +0000

    Add

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -92,6 +92,13 @@ class TestRectangle(unittest.TestCase):
 
         self.assertEqual(str(ve.exception), "x must be >= 0")
 
+    def test_rect_val_w2(self):
+        """Test Rect 15"""
+        with self.assertRaises(ValueError) as ve:
+            b4 = Rectangle(-1, 2)
+
+        self.assertEqual(str(ve.exception), "width must be > 0")
+
     def test_rect_val_y(self):
         """Test Rect 12"""
         with self.assertRaises(ValueError) as ve:
@@ -105,10 +112,13 @@ class TestRectangle(unittest.TestCase):
         b2 = b1.area()
         self.assertEqual(b2, 2)
 
+
+"""
     def test_rect_str(self):
-        """Test str 14"""
+        Test str 14
         r1 = Rectangle(4, 6, 2, 1, 12)
         self.assertEqual(str(r1), "[Rectangle] (12) 2/1 - 4/6")
+"""
 
 
 if __name__ == "__main__":
