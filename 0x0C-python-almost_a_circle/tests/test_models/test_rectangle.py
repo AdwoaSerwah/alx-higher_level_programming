4331294a8d655b3ba9c126ffd56939997b6dfdaf
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-01-16 20:18:07 +0000

    Add 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -24,22 +24,30 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(str(te.exception), "width must be an integer")
 
     def test_rect_height(self):
-        with self.assertRaises(TypeError) as te:
+        with self.assertRaises(TypeError) as ce:
             b3 = Rectangle(1, "2")
 
-        self.assertEqual(str(te.exception), "height must be an integer")
+        self.assertEqual(str(ce.exception), "height must be an integer")
 
     def test_rect_x(self):
-        with self.assertRaises(TypeError) as te:
+        with self.assertRaises(TypeError) as se:
             b4 = Rectangle(1, 2, "3")
 
-        self.assertEqual(str(te.exception), "x must be an integer")
+        self.assertEqual(str(se.exception), "x must be an integer")
 
     def test_rect_x(self):
-        with self.assertRaises(TypeError) as te:
+        with self.assertRaises(TypeError) as ve:
             b4 = Rectangle(1, 2, 3, "4")
 
-        self.assertEqual(str(te.exception), "y must be an integer")
+        self.assertEqual(str(ve.exception), "y must be an integer")
+
+    def test_rect_correct(self):
+        b1 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(b1.id, 5)
+        self.assertEqual(b1.width, 1)
+        self.assertEqual(b1.height, 2)
+        self.assertEqual(b1.x, 3)
+        self.assertEqual(b1.y, 4)
 
 
 if __name__ == "__main__":
