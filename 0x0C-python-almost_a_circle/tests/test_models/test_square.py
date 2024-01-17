0c74dbebed2814433202c4da05d14ba18bf8344e
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-01-17 17:32:25 +0000

    Add task

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -35,13 +35,49 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(b1.x, 2)
         self.assertEqual(b1.y, 3)
 
-    def test_sq_three(self):
-        """Test Square 3"""
+    def test_sq_four(self):
+        """Test Square 4"""
         b1 = Square(1, 2, 3, 86)
         self.assertEqual(b1.size, 1)
         self.assertEqual(b1.x, 2)
         self.assertEqual(b1.y, 3)
         self.assertEqual(b1.id, 86)
+
+    def test_sq_size_type(self):
+        """5"""
+        with self.assertRaises(TypeError) as te:
+            b1 = Square("1")
+        self.assertEqual(str(te.exception), "width must be an integer")
+
+    def test_sq_x_type(self):
+        """6"""
+        with self.assertRaises(TypeError) as te:
+            b1 = Square(1, "2")
+        self.assertEqual(str(te.exception), "x must be an integer")
+
+    def test_sq_y_type(self):
+        """7"""
+        with self.assertRaises(TypeError) as te:
+            b1 = Square(1, 2, "3")
+        self.assertEqual(str(te.exception), "y must be an integer")
+
+    def test_sq_size_val(self):
+        """8"""
+        with self.assertRaises(ValueError) as ve:
+            b1 = Square(-1)
+        self.assertEqual(str(ve.exception), "width must be > 0")
+
+    def test_sq_x_val(self):
+        """9"""
+        with self.assertRaises(ValueError) as ve:
+            b1 = Square(1, -2)
+        self.assertEqual(str(ve.exception), "x must be >= 0")
+
+    def test_sq_y_val(self):
+        """10"""
+        with self.assertRaises(ValueError) as ve:
+            b1 = Square(1, 2, -3)
+        self.assertEqual(str(ve.exception), "y must be >= 0")
 
 
 if __name__ == "__main__":
