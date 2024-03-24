4360d228c624753986cc3e1700be66f76bd66fc6
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-03-24 16:43:35 +0000

    Delete javascript projects

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const lenList = list.length - 1;
-  const reversedList = [];
-  for (let i = lenList; i >= 0; i--) {
-    reversedList.push(list[i]);
-  }
-  return reversedList;
-};
