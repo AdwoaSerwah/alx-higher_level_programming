f82e9091a2aba668bf065233730c4870432d1d1e
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-05-22 21:45:15 +0000

    Add task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,5 @@ SELECT tv_genres.name AS genre,
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
