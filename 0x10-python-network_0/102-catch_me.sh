f81850dce47252acfbf52e58342f77040a997c2a
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-12 20:00:07 +0000

    Add 9

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Makes a request to 0.0.0.0:5000/catch_me to receive a response "You got me!"
+curl -s -X PUT -H "Content-Type: application/json" -d '{"user_id": 98}' 0.0.0.0:5000/catch_me
