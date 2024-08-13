fc6544a681a0be60d936b4fbb70414eb62ef0eff
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-13 02:51:27 +0000

    Add task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email, sends a POST request to the URL
+with the email as a parameter, and displays the response body
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    sk_url = sys.argv[1]
+    email = sys.argv[2]
+    
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    
+    with urllib.request.urlopen(sk_url, data) as response:
+        print(response.read().decode('utf-8'))
