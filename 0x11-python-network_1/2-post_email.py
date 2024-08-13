e35107bae5bc0299d6dcf46c285689cc58c73fbb
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-13 02:55:46 +0000

    Add task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,6 @@ import sys
 if __name__ == "__main__":
     sk_url = sys.argv[1]
     email = sys.argv[2]
- 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
     with urllib.request.urlopen(sk_url, data) as response:
