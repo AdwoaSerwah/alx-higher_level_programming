d68e3b94c19206e50f9e3ff0943583e0a7ddf3ef
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-26 05:18:35 +0000

    Add task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,51 @@
 #!/usr/bin/node
 const request = require('request');
+const { promisify } = require('util');
+
+const requestAsync = promisify(request);
+
+// Function to fetch a character's name
+const fetchCharacterName = async (url) => {
+  try {
+    const { body } = await requestAsync(url);
+    const character = JSON.parse(body);
+    return character.name;
+  } catch (error) {
+    console.error('Error fetching character data:', error);
+    return null;
+  }
+};
+
+// Function to fetch movie data and print characters
+const fetchMovieData = async (movieId) => {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  try {
+    const { body } = await requestAsync(apiUrl);
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+
+    if (characterUrls && characterUrls.length > 0) {
+      // Print the characters part of the API response
+      // console.log('Characters URLs:');
+      // console.log(characterUrls);
+
+      // console.log('\nCharacter Names:');
+      // Fetch and print characters in the same order as the URLs
+      for (const url of characterUrls) {
+        const name = await fetchCharacterName(url);
+        if (name) {
+          console.log(name);
+        }
+      }
+    } else {
+      console.log('No characters found');
+    }
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+    process.exit(1);
+  }
+};
 
 // Check for the correct number of arguments
 if (process.argv.length !== 3) {
@@ -9,43 +55,4 @@ if (process.argv.length !== 3) {
 
 // Get the Movie ID from command-line argument
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Function to fetch character data
-const fetchCharacter = (url, callback) => {
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error fetching the character:', error);
-      return;
-    }
-    try {
-      const character = JSON.parse(body);
-      callback(character.name);
-    } catch (err) {
-      console.error('Error parsing JSON:', err);
-    }
-  });
-};
-
-// Make the HTTP request to get the movie details
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching the movie data:', error);
-    process.exit(1);
-  }
-
-  try {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    // Fetch and print all character names
-    characterUrls.forEach((url, index) => {
-      fetchCharacter(url, (name) => {
-        console.log(name);
-      });
-    });
-  } catch (err) {
-    console.error('Error parsing movie JSON:', err);
-    process.exit(1);
-  }
-});
+fetchMovieData(movieId);
