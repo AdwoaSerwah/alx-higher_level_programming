d462a621bbbc702c9bd1180b868e9dfc48dc97e4
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-08-26 04:45:36 +0000

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,13 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-// Get the API URL from the first argument
-const apiUrl = process.argv[2];
-const wedgeAntillesId = '18';
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
